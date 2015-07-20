c88c588a421f9253a006c827116e03d470ce3ba7
oleksanderkorn <oleksanderkorn@gmail.com>
2015-07-20 02:02:40 +0200

    add pagination of search results

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,57 +6,6 @@ $(document).ready(function () {
         );
     });
 
-    $(document).on('click', '#search', function () {
-        if ($('#query').val() != '') {
-            $('#movies').empty();
-            var pages = 1;
-            $.get("/movies?query=" + $('#query').val(), function (data) {
-                pages = data.total_pages;
-                var addButton, form, movieDiv, movieTitleDiv;
-                var movies = $('#movies');
-                var movieResults = data.results;
-                $.each(movieResults, function (index, movie) {
-                    var posterPath = movie.poster_path == null
-                        ? "/assets/images/noposter.png"
-                        : "http://image.tmdb.org/t/p/w92" + movie.poster_path;
-                    var img = $("<img name='posterPath' src='" + posterPath + "'>");
-                    form = $("<form action='/movie' method='post'>");
-                    movieDiv = $("<div class='movie'>");
-                    addButton = $("<div class='favorite-button-add'>");
-                    movieTitleDiv = $("<div class='movie-title'>").text(movie.title);
-                    movieDiv.append(addButton);
-                    movieDiv.append(movieTitleDiv);
-                    movieDiv.append(img);
-                    form.append(movieDiv);
-                    movies.append(form);
-                });
-                //append search results for next pages of api request, but limit max pages to 10
-                for (var i = 2; i <= pages && i <= 10; i++) {
-                    $.get("/movies?query=" + $('#query').val() + "&page=" + i, function (nextData) {
-                        movieResults = nextData.results;
-                        $.each(movieResults, function (nextIndex, nextMovie) {
-                            var posterPath = nextMovie.poster_path == null
-                                ? "/assets/images/noimg.png"
-                                : "http://image.tmdb.org/t/p/w92" + nextMovie.poster_path;
-                            var img = $("<img name='posterPath' src='" + posterPath + "'>");
-                            form = $("<form action='/movie' method='post'>");
-                            movieDiv = $("<div class='movie'>");
-                            addButton = $("<div class='favorite-button-add'>");
-                            movieTitleDiv = $("<div class='movie-title'>").text(nextMovie.title);
-                            movieDiv.append(addButton);
-                            movieDiv.append(movieTitleDiv);
-                            movieDiv.append(img);
-                            form.append(movieDiv);
-                            movies.append(form);
-                        });
-                    });
-                }
-            });
-        } else {
-            alert("Insert a search query, please.");
-        }
-    });
-
     $(document).on('click', '#openList', function () {
         var that = $(this);
         var data = {listId: $('#favoriteList').find(":selected").attr("listId")};
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,10 +1,56 @@
-angular.module('themoviedbApp', []).controller('MoviesCtrl', function ($scope) {
+angular.module('themoviedbApp', ['ui.bootstrap']).controller('MoviesCtrl', function ($scope, $log) {
+    $scope.currentSearchText;
     $scope.movieList = [];
-    $scope.searchMovies = function() {
-        $.get('/movies?query=' + $('#query').val(), function (data) {
-            $.each(data.results, function(i, item){
-                $scope.movieList.push({"title" :item.title , "poster_path" : item.poster_path});
-            });
+    $scope.currentPage = 1;
+    $scope.totalItems = 0;
+    $scope.maxSize = 15;
+    $scope.itemsPerPage = 20;
+    $scope.maxSearchResult = 20000;
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+    $scope.changePage = function () {
+        $scope.movieList = [];
+        $.get("/movies?query=" + $scope.currentSearchText + "&page=" + $scope.currentPage, function (data) {
+            $scope.handleSearchResults(data);
+            $scope.$apply();
         });
     };
+
+    $scope.searchMovies = function () {
+        $('.noSearchValue').hide();
+        $scope.currentPage = 1;
+        $scope.currentSearchText = $('#query').val();
+        if ($scope.currentSearchText) {
+            $scope.movieList = [];
+            $.get('/movies?query=' + $scope.currentSearchText, function (data) {
+                $scope.handleSearchResults(data);
+                $scope.totalItems = data.total_pages * $scope.movieList.length <= $scope.maxSearchResult
+                    ? data.total_pages * $scope.movieList.length : $scope.maxSearchResult;
+                $scope.$apply();
+            });
+        } else {
+            $('.noSearchValue').show();
+        }
+    };
+
+    $scope.handleSearchResults = function (data) {
+        $.each(data.results, function (i, item) {
+            var imgSrc;
+            if (item.poster_path != null) {
+                imgSrc = "http://image.tmdb.org/t/p/w92" + item.poster_path;
+            } else {
+                imgSrc = "/assets/images/noimg.png";
+            }
+            $scope.movieList.push({"title": item.title, "poster_path": imgSrc});
+        });
+    };
+
+    $scope.addToFavorite = function () {
+
+    };
+
+
 });
