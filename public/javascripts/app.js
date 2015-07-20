00b1e53d636624574f5f62e359192f54f853397e
oleksanderkorn <oleksanderkorn@gmail.com>
2015-07-20 04:08:22 +0200

    migrate favorite list creation/reading to angular js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    $.get("/lists", function (lists) {
-        return $.each(lists, function (i, list) {
-                $('#favoriteList').append("<option value='" + i + "' listId='" + list.listId + "'>" + list.listName + "</option>");
-            }
-        );
-    });
-
     $(document).on('click', '#openList', function () {
         var that = $(this);
         var data = {listId: $('#favoriteList').find(":selected").attr("listId")};
@@ -14,26 +7,4 @@ $(document).ready(function () {
             location.href = "/favorite?listId=" + list.listId;
         });
     });
-
-    $(document).on('change', '#favoriteList', function () {
-        $('.favorite-button-add').removeClass('favorite-button-remove');
-    });
-
-    $(document).on('click', '.favorite-button-add', function () {
-        if($('#favoriteList').val() != null) {
-            var that = $(this);
-            var data = {
-                title: that.next().html(),
-                posterPath: that.next().next().attr("src"),
-                listId: $('#favoriteList').find(":selected").attr("listId")
-            };
-            $.post("/movie", data);
-            $(this).addClass('favorite-button-remove');
-        } else {
-            alert("Create a list first, please.");
-        }
-    });
 });
-
-
-
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,4 +1,4 @@
-angular.module('themoviedbApp', ['ui.bootstrap']).controller('MoviesCtrl', function ($scope, $log) {
+angular.module('themoviedbApp', ['ui.bootstrap']).controller('MoviesCtrl', function ($scope, $http) {
     $scope.currentSearchText;
     $scope.movieList = [];
     $scope.currentPage = 1;
@@ -6,6 +6,7 @@ angular.module('themoviedbApp', ['ui.bootstrap']).controller('MoviesCtrl', funct
     $scope.maxSize = 15;
     $scope.itemsPerPage = 20;
     $scope.maxSearchResult = 20000;
+    $scope.favoriteList;
 
     $scope.setPage = function (pageNo) {
         $scope.currentPage = pageNo;
@@ -42,15 +43,62 @@ angular.module('themoviedbApp', ['ui.bootstrap']).controller('MoviesCtrl', funct
             if (item.poster_path != null) {
                 imgSrc = "http://image.tmdb.org/t/p/w92" + item.poster_path;
             } else {
-                imgSrc = "/assets/images/noimg.png";
+                imgSrc = "/assets/images/noposter.png";
             }
             $scope.movieList.push({"title": item.title, "poster_path": imgSrc});
         });
     };
 
-    $scope.addToFavorite = function () {
-
+    $scope.readFavoriteList = function () {
+        $.get("/lists", function (lists) {
+            $scope.favoriteList = lists;
+            $scope.$apply();
+        });
     };
 
+    $scope.addMovieToFavorites = function ($index, mov) {
+        if($('#favoriteList').val() != null) {
+            var data = {
+                title: mov.title,
+                posterPath: mov.poster_path,
+                listId: $('#favoriteList').find(":selected").attr("listId")
+            };
+            console.log(data);
+            $http({
+                method: 'POST',
+                url: "/movie",
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: data
+            });
+        } else {
+            alert("Create a list first.");
+        }
+    };
 
+    $scope.createList = function () {
+        $http({
+            method: 'POST',
+            url: "/list",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj)
+                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                return str.join("&");
+            },
+            data: {listName : $("input[name='listName']").val()}
+        }).success(function() {
+            $scope.readFavoriteList();
+        });
+    };
+
+    $scope.openFavoriteList = function () {
+
+    };
 });
