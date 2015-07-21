67ed9ba6d87ce8e3d0a70f59b9c38920f2f3574d
oleksanderkorn <oleksanderkorn@gmail.com>
2015-07-21 23:21:13 +0200

    Refactor Application controller method names.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,17 +20,18 @@ public class Application extends Controller {
     @Inject
     private WSClient ws;
 
-    public static final String SEARCH_MOVIES_URL = "http://api.themoviedb.org/3/search/movie?api_key=fb92ed0bac9cfafe54830e26abe791df&query=";
-    public static final String QUERY = "query";
-    public static final String PAGE = "page";
+    public static final String SEARCH_MOVIES_URL_API_KEY = "http://api.themoviedb.org/3/search/movie?api_key=fb92ed0bac9cfafe54830e26abe791df&query=";
+    public static final String QUERY_KEY = "query";
+    public static final String PAGE_KEY = "page";
 
-    public F.Promise<Result> searchMovies() {
-        String query = Form.form(String.class).bindFromRequest().data().get(QUERY) != null
-                ? Form.form(String.class).bindFromRequest().data().get(QUERY).replace(" ", "+") : "";
-        query += Form.form(String.class).bindFromRequest().data().get(PAGE) != null
-                ? "&" + PAGE + "=" + Form.form(String.class).bindFromRequest().data().get(PAGE) : "";
+    public F.Promise<Result> getMovies() {
+        String query = Form.form(String.class).bindFromRequest().data().get(QUERY_KEY) != null
+                ? Form.form(String.class).bindFromRequest().data().get(QUERY_KEY).replace(" ", "+") : "";
 
-        F.Promise<Result> promise = ws.url(SEARCH_MOVIES_URL + query).get().map((r) -> {
+        query += Form.form(String.class).bindFromRequest().data().get(PAGE_KEY) != null
+                ? "&" + PAGE_KEY + "=" + Form.form(String.class).bindFromRequest().data().get(PAGE_KEY) : "";
+
+        F.Promise<Result> promise = ws.url(SEARCH_MOVIES_URL_API_KEY + query).get().map((r) -> {
             if (r.getStatus() == 200) {
                 return ok(r.asJson());
             } else {
@@ -41,19 +42,19 @@ public class Application extends Controller {
         return promise;
     }
 
-    public Result getListToOpen() {
+    public Result getFavoriteListById() {
         FavoriteList list = Form.form(FavoriteList.class).bindFromRequest().get();
         list = (FavoriteList) new Model.Finder(FavoriteList.class).byId(list.listId);
         return ok(toJson(list));
     }
 
-    public Result addList() {
+    public Result addFavoriteList() {
         FavoriteList favoriteList = Form.form(FavoriteList.class).bindFromRequest().get();
         favoriteList.save();
         return created();
     }
 
-    public Result addMovie() {
+    public Result addMovieToFavoriteList() {
         int listId = Integer.parseInt(Form.form(FavoriteMovie.class).bindFromRequest().data().get("listId"));
         FavoriteList list = (FavoriteList) new Model.Finder(FavoriteList.class).byId(listId);
         FavoriteMovie movie = Form.form(FavoriteMovie.class).bindFromRequest().get();
@@ -64,7 +65,7 @@ public class Application extends Controller {
         return created();
     }
 
-    public Result lists() {
+    public Result getFavoriteLists() {
         List<FavoriteList> lists = new Model.Finder(FavoriteList.class).all();
         return ok(toJson(lists));
     }
